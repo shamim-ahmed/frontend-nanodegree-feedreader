2b843c45c4f00b5676e3cb093c84ce03e8e0ea60
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-09-25 14:31:28 +1000

    - updated README and comments - updated the last asynchronous test to eliminate some unnecessary steps

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,20 +9,14 @@
 * to ensure they don't run until the DOM is ready.
 */
 $(function() {
-  // change the value of the timeout interval for asynchronous tests
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-
   /* This is our first test suite - a test suite just contains
   * a related set of tests. This suite is all about the RSS
   * feeds definitions, the allFeeds variable in our application.
   */
   describe('RSS Feeds', function() {
-    /* This is our first test - it tests to make sure that the
+    /* This test ensures that the
     * allFeeds variable has been defined and that it is not
-    * empty. Experiment with this before you get started on
-    * the rest of this project. What happens when you change
-    * allFeeds in app.js to be an empty array and refresh the
-    * page?
+    * empty.
     */
     it('are defined', function() {
       expect(allFeeds).toBeDefined();
@@ -30,7 +24,7 @@ $(function() {
     });
 
 
-    /* Write a test that loops through each feed
+    /* This test loops through each feed
     * in the allFeeds object and ensures it has a URL defined
     * and that the URL is not empty.
     */
@@ -46,7 +40,7 @@ $(function() {
     });
 
 
-    /* : Write a test that loops through each feed
+    /* This test loops through each feed
     * in the allFeeds object and ensures it has a name defined
     * and that the name is not empty.
     */
@@ -63,21 +57,21 @@ $(function() {
   });
 
 
-  /* Write a new test suite named "The menu" */
+  /* A new test suite named "The menu" */
   describe('The menu', function() {
-    /* Write a test that ensures the menu element is
-    * hidden by default. You'll have to analyze the HTML and
-    * the CSS to determine how we're performing the
-    * hiding/showing of the menu element.
+    /* This test verifies that the menu element is
+    * hidden by default. The menu element is hidden
+    * when the class attribute of body element contains 'menu-hidden'.
+    * Removing this class from the body makes the menu visible.
     */
     it('is hidden', function() {
       expect($("body.menu-hidden").size()).toBe(1);
     });
 
-    /* Write a test that ensures the menu changes
-    * visibility when the menu icon is clicked. This test
-    * should have two expectations: does the menu display when
-    * clicked and does it hide when clicked again.
+    /* This test ensures that the menu changes
+    * visibility when the menu icon is clicked. We simply test for the presence
+    * of 'menu-hidden' in the class attribtue of body to
+    * determine whether the menu is hidden or not.
     */
     it('changes visibility', function() {
       $("body > div.header > a.menu-icon-link").click();
@@ -89,10 +83,10 @@ $(function() {
 
   /* Write a new test suite named "Initial Entries" */
   describe('Initial Entries', function() {
-    /* Write a test that ensures when the loadFeed
+    /* This test ensures that when the loadFeed
     * function is called and completes its work, there is at least
     * a single .entry element within the .feed container.
-    * Remember, loadFeed() is asynchronous so this test will require
+    * loadFeed() is asynchronous, so this test requires
     * the use of Jasmine's beforeEach and asynchronous done() function.
     */
     beforeEach(function(done) {
@@ -109,38 +103,37 @@ $(function() {
 
   /* Write a new test suite named "New Feed Selection" */
   describe('New Feed Selection', function() {
-    /* Write a test that ensures when a new feed is loaded
-    * by the loadFeed function that the content actually changes.
-    * Remember, loadFeed() is asynchronous.
+    /* This test ensures that when a new feed is loaded
+    * by the loadFeed function, the content actually changes.
+    * loadFeed() is asynchronous, so this test requires
+    * the use of Jasmine's beforeEach and asynchronous done() function.
     */
-    var value1 = null;
-    var value2 = null;
-    var callbackFunction = null;
-
-    var firstValueReader = function() {
-      value1 = $(".feed .entry:first").text().trim();
-      loadFeed(1, secondValueReader);
-    };
-
-    var secondValueReader = function() {
-      value2 = $(".feed .entry:first").text().trim();
-
-      if (callbackFunction) {
-        callbackFunction();
-      }
-    };
+    var valueArray = [];
 
     beforeEach(function(done) {
-      callbackFunction = done;
-      loadFeed(0, firstValueReader);
+      // Read the value of all entries after loading from
+      // first source has been completed.
+      valueArray.push($(".feed .entry").text().trim());
+
+      // Reload the feeds from the second source.
+      // Read the value of all entries after loading from
+      // second source has been completed.
+      loadFeed(1, function() {
+        valueArray.push($(".feed .entry").text().trim());
+        done();
+      });
     });
 
     it('changes content', function(done) {
-      expect(value1).not.toBeNull();
-      expect(value1.length).toBeGreaterThan(0);
-      expect(value2).not.toBeNull();
-      expect(value2.length).toBeGreaterThan(0);
-      expect(value1).not.toEqual(value2);
+      // Compare the entry values from the two sources.
+      var firstValue = valueArray[0];
+      var secondValue = valueArray[1];
+
+      expect(firstValue).not.toBeNull();
+      expect(firstValue.length).toBeGreaterThan(0);
+      expect(secondValue).not.toBeNull();
+      expect(secondValue.length).toBeGreaterThan(0);
+      expect(firstValue).not.toEqual(secondValue);
       done();
     });
   });
